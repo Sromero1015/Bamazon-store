7b153f5236d22d90a94e91eb445a1df67bcb9385
Sergio Romero <sromerojr1015@gmail.com>
2019-11-14 04:54:38 -0500

    sql code

---
+++ bamazon.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  products_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  stock_quantity INT(21) NOT NULL,
+  PRIMARY KEY (item_id)
+);
+
+Select * FROM products;
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
+VALUES (101, "cutting board", "kitchen", 17.99, 20),
+(212, "external hard drive", "electronics", 69.99, 10),
+(313, "vinyl record player", "electronics", 58.99, 5),
+(420, "humidifier", "home", 79.99, 14),
+(504, "fishing rod", "outdoors", 49.99, 15),
+(619, "pressure cooker", "kitchen", 49.99, 19),
+(720, "basketball", "sports", 19.99, 19),
+(808, "bats", "sports", 44.99, 10),
+(913, "cushions", "home", 14.99, 14),
+(1009, "monitor", "electronics", 249.99, 17);
